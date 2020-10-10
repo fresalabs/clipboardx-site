14ccdf07cac9dce3f2cc3e9db4c4a29747669523
mohithg <mohithgm@gmail.com>
2020-10-10 13:03:09 +0000

    Updates

---
+++ precache-manifest.4da8ec980028ea35ed371ff8ce4548b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87bdf3bcd5e61344acdbf5555d7ce05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cb2a4ef61566729dcca",
+    "url": "/static/css/main.b181dda5.chunk.css"
+  },
+  {
+    "revision": "8f0577ae79783dae3347",
+    "url": "/static/js/2.71f3d661.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.71f3d661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb2a4ef61566729dcca",
+    "url": "/static/js/main.03ec90eb.chunk.js"
+  },
+  {
+    "revision": "9c1e9ae35da44d41383d",
+    "url": "/static/js/runtime-main.3863486f.js"
+  }
+]);
